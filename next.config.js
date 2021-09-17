2b1d7631f8b9c193f360df6d5381dc1b1be278cf
kentBui <duccuong.hpe@gmail.com>
2021-09-17 16:54:40 +0700

    add more next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_MAPBOX_API_TOKEN:
+      "pk.eyJ1Ijoia2VudGJ1aSIsImEiOiJja2tmMmZncmYwMWx1Mm5ud244MHAyeXl1In0.V2nnOAQKO5O7t7mrg_eGJA",
+    NEXT_PUBLIC_API_URL: "https://event-date-server.herokuapp.com",
+    NEXT_PUBLIC_FRONTEND_URL: "https://eventdate.vercel.app",
+  },
   images: {
     domains: ["res.cloudinary.com"],
   },
